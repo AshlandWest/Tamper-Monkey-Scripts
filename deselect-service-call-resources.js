8bd1d335ca423f315bd6f7f392f2aa22b57e0b6c
Ash West <ashlandmwest@gmail.com>
2025-09-16 10:14:19 -0700

    Add Deselect Service Call Resources script

---
+++ deselect-service-call-resources.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Deselect Service Call Resources
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Deselect all the resource checkboxes from a new service call.
+// @author       Ash
+// @match        https://ww5.autotask.net/autotask/views/ServiceDesk/ServiceDeskTicket/service_ticket_panel_edit.aspx?ticketRenderType=ServiceCall&*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  const resourceTable = document.getElementsByClassName(
+    "service_call_render_resource_table"
+  )[0];
+  const resourceCheckboxes = resourceTable.getElementsByTagName("input");
+  Array.prototype.forEach.call(
+    resourceCheckboxes,
+    (inputElement) => (inputElement.checked = false)
+  );
+})();
